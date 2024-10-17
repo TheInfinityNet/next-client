dbd296f4b029a9bc01003e1cebe27cc1e1d89a2b
Nguyen Minh Tuan <tnowad@gmail.com>
2024-10-17 11:35:34 +0700

    feat: add sign-up page

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,262 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useToast } from "@/hooks/use-toast";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import {
+  signUpRequestSchema,
+  type SignUpRequestSchema,
+} from "@/lib/api/schemas/sign-up-request-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { mapFieldErrorToFormError } from "@/lib/utils";
+import { useSignUpMutation } from "@/hooks/apis/auth";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function SignUpPage() {
+  const router = useRouter();
+  const { toast } = useToast();
+  const signUpMutation = useSignUpMutation();
+
+  const signUpForm = useForm<SignUpRequestSchema>({
+    resolver: zodResolver(signUpRequestSchema),
+    defaultValues: {
+      firstName: "Tuan",
+      middleName: "Minh",
+      lastName: "Nguyen",
+      email: "tnowad@gmail.com",
+      password: "Password@123",
+      passwordConfirmation: "Password@123",
+      mobileNumber: "0123456789",
+      birthdate: "10/12/2003",
+      gender: "Male",
+      acceptTerms: false,
+    },
+  });
+
+  const onSubmit = signUpForm.handleSubmit((values) => {
+    signUpMutation.mutate(values, {
+      onSuccess: () => {
+        toast({
+          title: "Sign up successful",
+          description: "You have been signed up",
+        });
+        router.push("/sign-in");
+      },
+      onError: (error) => {
+        toast({
+          title: "Sign up failed",
+          description: error.message,
+        });
+        if (error.type === "ValidationError") {
+          mapFieldErrorToFormError(signUpForm.setError, error.errors);
+        }
+      },
+    });
+  });
+
+  return (
+    <Card className="mx-auto w-full max-w-screen-md space-y-6">
+      <CardHeader>
+        <CardTitle>Sign Up</CardTitle>
+        <CardDescription>
+          Create your account by filling out the form below.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <Form {...signUpForm}>
+          <form onSubmit={onSubmit} className="grid md:grid-cols-2 gap-x-2">
+            <FormField
+              control={signUpForm.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="First Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={signUpForm.control}
+              name="middleName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Middle Name (optional)</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Middle Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={signUpForm.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Last Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={signUpForm.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={signUpForm.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={signUpForm.control}
+              name="passwordConfirmation"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password Confirmation</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Confirm Password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={signUpForm.control}
+              name="mobileNumber"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Mobile Number</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Mobile Number" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={signUpForm.control}
+              name="birthdate"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Birthdate</FormLabel>
+                  <FormControl>
+                    <Input type="date" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={signUpForm.control}
+              name="gender"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Gender</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Gender" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="Male">Male</SelectItem>
+                      <SelectItem value="Female">Female</SelectItem>
+                      <SelectItem value="Other">Other</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={signUpForm.control}
+              name="acceptTerms"
+              render={({ field }) => (
+                <FormItem className="col-span-full">
+                  <div className="items-top flex space-x-2 mt-2">
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <FormLabel>Accept Terms</FormLabel>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              className="w-full mt-4"
+              type="submit"
+              disabled={signUpMutation.isPending}
+            >
+              Sign Up
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Link href="/sign-in" className="text-sm hover:text-primary">
+          Already have an account? Sign In
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+}
